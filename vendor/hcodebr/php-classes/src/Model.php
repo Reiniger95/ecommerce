01c9fea5d83935f0a797b67be60cf19694669ca9
reiniger95 <reiniger.95@gmail.com>
2021-04-20 17:42:57 -0300

    categorias

--- vendor/hcodebr/php-classes/src/Model.php
+++ vendor/hcodebr/php-classes/src/Model.php
@@ -14,7 +14,7 @@ $fieldName = substr($name, 3 , strlen($name));
 switch ($method)
 {
     case "get":
-return $this->values[$fieldName];
+return (isset($this->values[$fieldName])) ? $this->values[$fieldName] : NULL;
 break;
 
 case "set":
